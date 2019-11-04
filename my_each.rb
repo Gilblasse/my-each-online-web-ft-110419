cd494730a9d964608125bb79371744b878246a38
Nethelbert <gilblasse@gmail.com>
2019-11-04 23:22:39 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,8 +1,8 @@
 def my_each (array)
-  
-  i = 0 
-  while i < array.size do
-    yield(array[i])
-    i += 1
-  end
+  if array.size != 0
+    i = 0 
+    while i < array.size do
+      yield(array[i])
+      i += 1
+    end
 end
